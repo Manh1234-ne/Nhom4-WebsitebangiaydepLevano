90cd859109c380ef93d81cf93fda78e3c4845ace
Manh1234-ne <manhnon16@gmail.com>
2026-03-11 09:50:13 +0700

    1.1 cắt giao diện

--- mvc-oop-basic/mvc-oop-basic/index.php
+++ mvc-oop-basic/mvc-oop-basic/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 // Require file Common
 require_once './commons/env.php'; // Khai báo biến môi trường
@@ -9,6 +9,7 @@ require_once './controllers/HomeController.php';
 
 // Require toàn bộ file Models
 require_once './models/Student.php';
+require_once './models/SanPham.php';
 
 // Route
 $act = $_GET['act'] ?? '/';
@@ -16,6 +17,12 @@ $act = $_GET['act'] ?? '/';
 // Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
 
 match ($act) {
-    // Trang chủ
-    
+    // Route
+    '/' => (new HomeController())->home(), // trường hợp đặc biệt
+
+    'trangchu' => (new HomeController())->trangChu(),
+    // BASE_URL/?act=trangchu
+
+    'danh-sach-san-pham' => (new HomeController())->danhSachSanPham(),
+    // BASE_URL/?act=danh-sach-san-pham
 };
