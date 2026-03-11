914a99635a223cf698c1340cc70c06f75144ac2f
Leanhtao11 <donglynhat@gmail.com>
2026-03-11 15:44:11 +0700

    update my feature

---
+++ mvc-oop-basic/admin/models/AdminDonHang.php
@@ -0,0 +1,42 @@
+<?php
+class AdminDonHang
+{
+    public $conn;
+    public function __construct()
+    {
+        $this->conn =  connectDB();
+    }
+    public function getAllDonhang()
+    {
+        try {
+            $sql = 'SELECT don_hangs.*, trang_thai_don_hangs.ten_trang_thai FROM don_hangs INNER JOIN trang_thai_don_hangs ON don_hangs.trang_thai_id = trang_thai_don_hangs.id';
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll();
+        } catch (Exception $e) {
+            echo "Lỗi" . $e->getMessage();
+        }
+    }
+    public function getDetailDonhang($id)
+    {
+        try {
+            $sql = 'SELECT * FROM chi_tiet_don_hangs WHERE don_hangs.id = :id';
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([':id' => $id]);
+            return $stmt->fetchAll();
+        } catch (Exception $e) {
+            echo "Lỗi" . $e->getMessage();
+        }
+    }
+    public function getListSpDonHang($id)
+    {
+        try {
+            $sql = 'SELECT don_hangs.*, trang_thai_don_hangs.ten_trang_thai FROM don_hangs INNER JOIN trang_thai_don_hangs ON don_hangs.trang_thai_id = trang_thai_don_hangs.id WHERE don_hangs.id = :id';
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute([':id' => $id]);
+            return $stmt->fetchAll();
+        } catch (Exception $e) {
+            echo "Lỗi" . $e->getMessage();
+        }
+    }
+}
