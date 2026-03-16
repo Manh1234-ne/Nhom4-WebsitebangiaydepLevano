5d33e2bb39eec520778dc2b91969e2ea6dbde13c
hoa2kzedtx-beep <hoa2kzedtx@gmail.com>
2026-03-16 16:47:06 +0700

    giohang

--- mvc-oop-basic/index.php
+++ mvc-oop-basic/index.php
@@ -10,6 +10,7 @@ require_once './controllers/HomeController.php';
 // Require toàn bộ file Models
 require_once './models/Student.php';
 require_once './models/SanPham.php';
+require_once './models/GioHang.php';
 
 // Route
 $act = $_GET['act'] ?? '/';
@@ -20,12 +21,11 @@ $act = $_GET['act'] ?? '/';
 match ($act) {
     '/' => (new HomeController())->home(),
     'trangchu' => (new HomeController())->trangchu(),
-    // Route
-    '/' => (new HomeController())->home(), // trường hợp đặc biệt
-    'trangchu' => (new HomeController())->trangChu(),
-    // BASE_URL/?act=trangchu
+
 
     'danh-sach-san-pham' => (new HomeController())->danhSachSanPham(),
-    // BASE_URL/?act=danh-sach-san-pham
     'chi-tiet-san-pham' => (new HomeController())->chiTietSanPham(),
+    'them-gio-hang' => (new HomeController())->addGioHang(),
+    'gio-hang' => (new HomeController())->gioHang(),
+    // 'thanh-toan' => (new HomeController())->thanhToan(),
 };
